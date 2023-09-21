ee9a0a4a374f5bbfd89c8d40798e65fea9d18ec9
Enzo <enzo_fischer10@hotmail.com>
2023-09-21 13:33:26 -0300

    Refatorando e deixando o codigo mais claro

--- Pong.js
+++ Pong.js
@@ -1,9 +1,11 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 15;
+let raio = diametro / 2;
 
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
+
 
 
 function setup() {
@@ -12,16 +14,30 @@ function setup() {
 
 function draw(){
   background(0);
+  showBall();
+  moveBall();
+  checkCollision();
+}
+
+function showBall(){
   //circle(0, 0, 50); //x= 0, y= 0, diametro= 50
   circle(xBolinha, yBolinha, diametro);
+}
+
+function moveBall(){
   xBolinha += velocidadeXBolinha; // ou xBolinha = + velocidadeXBolinha
   yBolinha += velocidadeYBolinha;
-  
-  if (xBolinha > width || xBolinha < 0){ //se a bolinha bater na borda volte
+}
+
+function checkCollision(){
+  if (xBolinha + raio > width || xBolinha - raio < 0){ //se a bolinha bater na borda volte
     velocidadeXBolinha *= -1;
   }
   
-  if (yBolinha > height || yBolinha < 0){
+  //x da bolinha eh o centro da bolinha ent temos que pegar o raio dela
+  //para efetuar de forma eficaz, a batida na borda, sem entrar metade da bolinha
+  
+  if (yBolinha + raio > height || yBolinha - raio < 0){
     velocidadeYBolinha *= -1;
   }
 }
